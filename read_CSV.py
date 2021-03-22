c0d5696756eb65aaeb86dd69ad1870812688563b
Sumit Sahaje <ssahaje@gmail.com>
2021-03-23 00:20:59 +0530

    new_file_main_branch_Edited_via_pycharm_and_pushed_to_git_branch1

--- read_CSV.py
+++ read_CSV.py
@@ -8,3 +8,20 @@ with open('Users.csv', 'r') as f:
 
         for line in csv_reader:
             csv_writer.writerow(line)
+
+with open('Users.csv', 'r') as f:
+    csv_reader = csv.DictReader(f)
+
+    for line in csv_reader:
+        print(line['Email'])
+
+with open('Users.csv', 'r') as f:
+    csv_reader = csv.DictReader(f)
+
+    with open('DitcWriter_New_Users.csv', 'w', newline='') as new_dict_writer_file:
+        field_names = ['Fname', 'Lname', 'Email']
+        csv_writer = csv.DictWriter(new_dict_writer_file, fieldnames=field_names, delimiter='|')
+
+        csv_writer.writeheader()
+        for line in csv_reader:
+            csv_writer.writerow(line)
